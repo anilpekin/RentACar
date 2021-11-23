501f7a639103900d72f171af240523b63749ee0f
anıl pekin <anlpek1n@gmail.com>
2021-11-23 20:41:57 +0300

    12. Gün ödev

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(p => p.CarName).NotEmpty();
+            RuleFor(p => p.CarName).MinimumLength(2).WithMessage("Car name must be greater than 2 digits");
+            RuleFor(p => p.DailyPrice).NotEmpty();
+            RuleFor(p => p.DailyPrice).GreaterThan(0).WithMessage("Dailyprice must be greater than 0");
+        }
+    }
+}
